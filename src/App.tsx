dfd92c4aed3aa2b529c354a5d142072003f5ba78
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2022-05-26 14:15:28 +0300

    add router and multi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import { Tile } from './Component/Tile';
 
-function App() {
+
+import './App.css';
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SingleTile />}/>
+      <Route path="/many" element={<ManyTiles />}/>
+  
+    </Routes>
+  </BrowserRouter>
+  );
+}
+function SingleTile() {
   return (
     <div className="App">
-        <Tile countDownSeconds={30}/>
+        <Tile countDownSeconds={10}/>
     </div>
   );
 }
+function ManyTiles() {
+  const getRandomNumber =() => Math.floor(Math.random() * 39) + 1; // 1 - 40
+  const tilesData = Array(1000).fill(10)//.map(() => getRandomNumber());
+  return (
+    <div className="App" >
+      {tilesData.map((seconds) => <Tile countDownSeconds={seconds} />)}
+    </div>
+  );
+}
+
 
 export default App;
