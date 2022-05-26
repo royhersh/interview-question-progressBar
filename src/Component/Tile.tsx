04f8474e9c7e6faa881fb8dd0fc23f07ed0416f5
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2022-05-26 14:16:05 +0300

    solution with transform

--- src/Component/Tile.tsx
+++ src/Component/Tile.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useState, useEffect }  from 'react';
 import './tile.scss';
 
 type TileProps = {
@@ -6,11 +6,26 @@ type TileProps = {
 }
 
 export const Tile: React.FC<TileProps> = ({countDownSeconds}) => {
-  
+  const [timeToDisplay, setTimeToDisplay] = useState(countDownSeconds);
+  const [progress, setProgress] = useState(0);
+
+  const timer = setTimeout(() => { 
+    setTimeToDisplay(timeToDisplay - 1);
+  }, 1000);
+  if (timeToDisplay === 0) clearTimeout(timer);
+
+  useEffect(() => { 
+    requestAnimationFrame(() => {
+      setProgress(-100);
+    })
+  }, [])
+
   return (
     <div className="tile">
-      <div className='timer'>00:30</div>
-      <div className='progress-bar'></div>
+      <div className='timer'>{timeToDisplay}</div>
+      <div style={{overflow: 'hidden'}}>
+        <div className='progress-bar' style={{transform: `translateX(${progress}%)`, transition: `transform ${countDownSeconds}s linear`}}></div>
+      </div>
       <div>Lorem Ipsum</div>
     </div>
   );
