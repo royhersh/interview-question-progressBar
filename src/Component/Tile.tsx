9023e41cb8b8e294999ac65a21545d417a39e1f2
yohai.zvuloon@autodesk.com <yohai.zvuloon@autodesk.com>
2022-05-26 18:40:25 +0300

    first sulotion

--- src/Component/Tile.tsx
+++ src/Component/Tile.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useEffect, useState }  from 'react';
 import './tile.scss';
 
 type TileProps = {
@@ -6,11 +6,45 @@ type TileProps = {
 }
 
 export const Tile: React.FC<TileProps> = ({countDownSeconds}) => {
+
+  const start = 30;
+  const [width, setWidth] = useState(100);
+  const [timer, setTimer] = useState(start);
+
+  useEffect(()=>{
+    setTimeout(()=>{setWidth(0);},0);
+  },[]);
+
+  useEffect(()=>{
+
+    const intervalId = setInterval(()=>{
+      
+
+      setTimer(prevTimer=>{
+        if(prevTimer <= 0){
+          clearInterval(intervalId);
+          return 0;
+        }
+      console.log(prevTimer);
+        return prevTimer-1;
+      });
+
+    },1000);
+
+    return ()=>{
+      clearInterval(intervalId);
+    }
+  },[])
   
   return (
     <div className="tile">
-      <div className='timer'>00:30</div>
-      <div className='progress-bar'></div>
+      <div className='timer'>{timer}</div>
+      <div className='progress-bar' style={{
+         height: 5,
+         width: `${width}%`,
+         transition: `width ${30}s linear`,
+         backgroundColor: 'red'
+      }}></div>
       <div>Lorem Ipsum</div>
     </div>
   );
